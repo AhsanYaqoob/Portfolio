9074909b08e043d12a286da6d67567ff13e26eba
AhsanYaqoob <ahsanyaqoob2723@gmail.com>
2024-08-17 11:58:44 +0500

    Initial commit for portfolio

---
+++ src/constants.js
@@ -0,0 +1,46 @@
+
+export const services = [
+    {
+        title: "Tech Visionary",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    
+];
+
+export const name = 'Ahsan ';
+
+export const experiences = [
+    {
+        "company": "Yellow World Tech",
+        "role": "Junior Web Development Intern",
+        "duration": "July 2024 - Present",
+        "logo": "https://yellowworldtech.ae/wp-content/uploads/2024/03/cropped-logofull_yellow-transformed-70x60.webp",
+        "points": [
+            "Excited to start my role, where I'm looking forward to gaining valuable experiences and enhancing my web development skills.",
+            "Contributing to various web development projects, applying my knowledge and learning new technologies.",
+            "Collaborating with a dynamic team to develop innovative solutions and optimize the performance of web applications."
+        ],
+        "url": "https://yellowworldtech.ae/"
+    },
+    {
+        "company": "GDSC (Google Developer Student Clubs)",
+        "role": "Tech Team Member",
+        "duration": "Sep 2023 - May 2024",
+        "logo": "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/contentbuilder/GDG-Bevy-ChapterThumbnail.png",
+        "points": [
+            "Collaborated on various technical projects, contributing to the development of web applications and enhancing their performance.",
+            "Organized and participated in coding sessions, sharing knowledge and fostering a collaborative learning environment."
+        ],
+        "url": "https://developers.google.com/community/gdsc"
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_en9sfnp';
+export const EMAIL_JS_TEMPLATE_ID = 'template_2vs5s48';
+export const EMAIL_JS_PUBLIC_KEY = 'CcysBNWsGkh8Ch-CW';
